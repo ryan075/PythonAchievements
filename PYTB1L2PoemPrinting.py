61e83bc25d636a247204bf20e3746ac9a9b8f4b5
Ryan Tilleman <apple@MacBook-Pro.local>
2020-09-30 15:08:25 +0200

    PythonAchievements

---
+++ PYTB1L2PoemPrinting.py
@@ -0,0 +1,16 @@
+vers1 = "Ik ben Ryan."
+print (vers1)
+vers2 = "Ik woon in Krommenie al sinds een tijdje."
+print (vers2)
+vers3 = "Ik woon hier samen met mijn ouders dat is een feitje."
+print (vers3)
+vers4 = "Voor de rest chill ik graag met een meisje."
+print (vers4)
+vers5 = "Daarnaast doen veel mensen domme dingen voor een klein prijsje"
+print (vers5)
+vers6 = "Laatste tijd speel ik veel Minecraft in een grote wereld, maar ach joh klein reisje."
+print (vers6)
+vers7 = "Ik kickboks, en dat niet voor een kleine tijdje."
+print (vers7)
+vers8 = "Deze opdracht dacht veel tijd van me af te nemen maar nee ik vermijd je :)."
+print (vers8)
