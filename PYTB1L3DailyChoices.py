ae47bff7c76600b632b765de8fc84aa778e07173
Ryan Tilleman <apple@MacBook-Pro.local>
2020-10-07 15:05:13 +0200

    Daily Choises

---
+++ PYTB1L3DailyChoices.py
@@ -0,0 +1,68 @@
+score = 0
+
+# QUESTION 1
+answer1 = input("Wat is mijn naam? \n a. Sander \n b. Ryan \n c. Tijmen \n Answer: ")
+if answer1 == "b" or answer1 == "Ryan":
+	score += 1
+	print("Correct!")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Ryan.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 2
+answer1 = input("Op welke sport zit ik? \n a. Kickboksen \n b. Voetbal \n c. MMA \n Answer: ")
+if answer1 == "a" or answer1 == "Kickboksen":
+	score += 1
+	print("Correct!")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Kickboksen.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 3
+answer1 = input("Hoe oud ben ik? \n a. 15 \n b. 16 \n c. 17 \n Answer: ")
+if answer1 == "b" or answer1 == "16":
+	score += 1
+	print("Correct!")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is 16.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 4
+answer1 = input("Waar ben ik geboren? \n a. Thuis \n b. AMC \n c. ZMC \n Answer: ")
+if answer1 == "c" or answer1 == "ZMC":
+	score += 1
+	print("Correct!")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is ZMC.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 5
+answer1 = input("Op welke school zit ik? \n a. Mediacollege \n b. Trias VMBO \n c. Altra \n Answer: ")
+if answer1 == "a" or answer1 == "Media Colege":
+	score += 1
+	print("Correct!")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Mediacollege.")
+	print("Score: ", score)
+	print("\n")
+
+
+
+
+
+
+
